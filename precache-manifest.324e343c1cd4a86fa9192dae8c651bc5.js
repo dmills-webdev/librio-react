81f6cf7dfab4c1b3e83cdf404c53e7175ae646ba
crongle <gasmazk73@gmail.com>
2020-09-07 12:01:18 +0100

    Updates

---
+++ precache-manifest.324e343c1cd4a86fa9192dae8c651bc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85276e13b0295983cffff12416a8e337",
+    "url": "/librio-react/index.html"
+  },
+  {
+    "revision": "81f1bbcfe820e5386df2",
+    "url": "/librio-react/static/css/main.48890a1c.chunk.css"
+  },
+  {
+    "revision": "b60059661364987a3e4e",
+    "url": "/librio-react/static/js/2.1a2a5171.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/librio-react/static/js/2.1a2a5171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81f1bbcfe820e5386df2",
+    "url": "/librio-react/static/js/main.210a5239.chunk.js"
+  },
+  {
+    "revision": "493d822aa4158718f035",
+    "url": "/librio-react/static/js/runtime-main.b17a9e86.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/librio-react/precache-manifest.324e343c1cd4a86fa9192dae8c651bc5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/librio-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
